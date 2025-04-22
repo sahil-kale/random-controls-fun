b8209ed7aa6814f6df6dbb1bb7d8f6e454c77506
Sahil Kale <sahil.kale1@gmail.com>
2025-04-22 17:10:27 -0400

    add alphas

--- system_id/model_id.py
+++ system_id/model_id.py
@@ -7,19 +7,16 @@ from collections import deque
 C_TO_KELVIN_OFFSET = 273.15
 
 class ThermalModel:
-    def __init__(self, h1, h2, h3, Tf, dead_time=35):
+    def __init__(self, h1, h2, h3, Tf, alpha_1, alpha_2):
         self.h1 = h1
         self.h2 = h2
         self.h3 = h3
         self.Tf = Tf
-        self.dead_time = dead_time
+        self.alpha_1 = alpha_1
+        self.alpha_2 = alpha_2
 
         self.construct_system_dynamics()
         self.X = np.ones((2, 1)) * Tf
-
-        self.input_history = deque()  # stores tuples of (timestamp, U)
-        # add in the initial input
-        self.input_history.append((0, np.array([0, 0, Tf]).reshape(3, 1)))
 
     def construct_system_dynamics(self):
         surface_area = 1e-3  # m^2
@@ -38,8 +35,8 @@ class ThermalModel:
         self.A = A / (heat_capacity * mass)
 
         B = np.array([
-            [1/100, 0, 1/(r1)],
-            [0, 1/100, 1/(r2)]
+            [self.alpha_1, 0, 1/(r1)],
+            [0, self.alpha_2, 1/(r2)]
         ])
         self.B = B / (heat_capacity * mass)
 
@@ -49,27 +46,15 @@ class ThermalModel:
     def propagate_dynamics(self, Q1_pct, Q2_pct, current_time, dt):
         # Buffer the current input
         U_current = np.array([Q1_pct, Q2_pct, self.Tf]).reshape(3, 1)
-        self.input_history.append((current_time, U_current))
-
-        # Find delayed input
-        delayed_input = self.input_history[0][1]  # Default to oldest
-        for t, u in reversed(self.input_history):
-            if current_time - t >= self.dead_time:
-                delayed_input = u
-                break
-
-        # Prune history (keep only relevant recent entries)
-        while len(self.input_history) > 2 and current_time - self.input_history[1][0] >= self.dead_time:
-            self.input_history.popleft()
 
         # RK4 integration
         NUM_RK_ITERATIONS = 2
         dt_step = dt / NUM_RK_ITERATIONS
         for _ in range(NUM_RK_ITERATIONS):
-            k1 = self.calculate_xdot(self.X, delayed_input)
-            k2 = self.calculate_xdot(self.X + dt_step/2 * k1, delayed_input)
-            k3 = self.calculate_xdot(self.X + dt_step/2 * k2, delayed_input)
-            k4 = self.calculate_xdot(self.X + dt_step * k3, delayed_input)
+            k1 = self.calculate_xdot(self.X, U_current)
+            k2 = self.calculate_xdot(self.X + dt_step/2 * k1, U_current)
+            k3 = self.calculate_xdot(self.X + dt_step/2 * k2, U_current)
+            k4 = self.calculate_xdot(self.X + dt_step * k3, U_current)
             self.X = self.X + (dt_step / 6) * (k1 + 2*k2 + 2*k3 + k4)
 
         return self.X
@@ -89,8 +74,8 @@ def main():
     Tf = 29 + C_TO_KELVIN_OFFSET
 
     def objective(x):
-        h1, h2, h3 = x
-        model = ThermalModel(h1, h2, h3, Tf, dead_time=35)
+        h1, h2, h3, alpha_1, alpha_2 = x
+        model = ThermalModel(h1, h2, h3, Tf, alpha_1, alpha_2)
         error_sum = 0
         for i in range(len(timestamps) - 1):
             dt = timestamps[i+1] - timestamps[i]
@@ -99,15 +84,15 @@ def main():
         return error_sum
 
     # Initial guess and bounds
-    initial_guess = [1000, 1000, 1000]
-    bounds = [(None, None), (0.1, None), (0.1, None)]
+    initial_guess = [10, 10, 100, 1/100, 0.75/100]
+    bounds = [(1, 100), (1, 100), (1, 1000), (0.001, 0.015), (0.001, 0.015)]
     result = minimize(objective, initial_guess, bounds=bounds, method='Nelder-Mead')
-    h1, h2, h3 = result.x
-    print(f"Optimized parameters: h1={h1:.3f}, h2={h2:.3f}, h3={h3:.6f}")
+    h1, h2, h3, alpha_1, alpha_2 = result.x
+    print(f"Optimized Parameters: h1={h1}, h2={h2}, h3={h3}, alpha_1={alpha_1}, alpha_2={alpha_2}")
     print(result)
 
     # Plotting
-    model = ThermalModel(h1, h2, h3, Tf, dead_time=35)
+    model = ThermalModel(h1, h2, h3, Tf, alpha_1, alpha_2)
     simulated_temperatures = []
     for i in range(len(timestamps) - 1):
         dt = timestamps[i+1] - timestamps[i]
