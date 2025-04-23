3d647644c33a82194b0ee2a56a4bbb3874041bda
Sahil Kale <sahil.kale1@gmail.com>
2025-04-23 16:46:33 -0400

    mrac with animation!

--- mrac-fun/mrac_part_2.py
+++ mrac-fun/mrac_part_2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.widgets import Slider
 
 class StateSpaceModel:
     def __init__(self, A, B, C, D):
@@ -11,95 +13,99 @@ class StateSpaceModel:
 
     def update(self, u, dt):
         x_dot = self.A @ self.state + self.B * u
-        self.state = x_dot * dt + self.state # simple euler integration
+        self.state = x_dot * dt + self.state
 
     def output(self):
         return self.C @ self.state + self.D
-    
+
 class MRACSimulator:
     def __init__(self, plant, reference_model, gamma):
         self.plant = plant
         self.reference_model = reference_model
         self.gamma = gamma
-        self.theta = np.zeros((2,)) # theta_r, theta_xp
+        self.theta = np.zeros((2,))
 
     def calculate_adaptive_control_weight(self, e, r, x_p, dt):
-        self.theta[0] -= np.array(self.gamma * e * r * dt)
-        self.theta[1] -= np.array(self.gamma * e * x_p * dt)
-        
+        self.theta[0] -= self.gamma * e * r * dt
+        self.theta[1] -= self.gamma * e * x_p * dt
+
     def get_controller_output(self, r, x_p):
         return self.theta[0] * r + self.theta[1] * x_p
 
-    def propogate_sim(self, r, u, dt):
+    def step(self, r, dt):
+        u = self.get_controller_output(r, self.plant.output())
         self.reference_model.update(r, dt)
         self.plant.update(u, dt)
+        e = self.plant.output() - self.reference_model.output()
+        self.calculate_adaptive_control_weight(e, r, self.plant.output(), dt)
+        return self.plant.output().item(), self.reference_model.output().item(), u, self.theta.copy()
 
-    def run_simulation(self, t, r, dt):
-        x_p = []
-        x_m = []
-        u = []
-        theta_hist = []
+if __name__ == "__main__":
+    A_p, B_p, C_p, D_p = np.array([[-1]]), np.array([[1]]), np.array([[1]]), np.array([[0]])
+    A_m, B_m, C_m, D_m = np.array([[-2]]), np.array([[2]]), np.array([[1]]), np.array([[0]])
 
-        for i in range(len(t)):
-            u_i = self.get_controller_output(r[i], self.plant.output())
-            u.append(u_i)
-            self.propogate_sim(r[i], u_i, dt)
-            x_p.append(self.plant.output())
-            x_m.append(self.reference_model.output())
-            e = x_p[-1] - x_m[-1]
-            self.calculate_adaptive_control_weight(e, r[i], x_p[i], dt)
-            theta_hist.append(self.theta.tolist())
-
-        return np.array(x_p), np.array(x_m), np.array(u), np.array(theta_hist)
-
-    def plot_results(self, t, x_p, x_m, u, theta_hist):
-        plt.figure(figsize=(12, 6))
-        plt.subplot(2, 1, 1)
-        plt.plot(t, x_m.reshape(-1), label='Reference Model $x_m$')
-        plt.plot(t, x_p.reshape(-1), label='Plant Output $x_p$', linestyle='--')
-        plt.title('MRAC with MIT Rule (1st-Order Plant)')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Output')
-        plt.legend()
-        
-        plt.subplot(2, 1, 2)
-        plt.plot(t, u.reshape(-1), label='Control Input $u$', color='orange')
-        theta_r = theta_hist[:, 0]
-        theta_xp = theta_hist[:, 1]
-        plt.plot(t, theta_r, label='Adaptive Parameter $\Theta_r$', linestyle='--')
-        plt.plot(t, theta_xp, label='Adaptive Parameter $\Theta_{xp}$', linestyle=':')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Control Input / Adaptive Parameter')
-        plt.legend()
-        
-        plt.tight_layout()
-        plt.show()
-
-if __name__ == '__main__':
-
-    A_p = np.array([-1]).reshape(-1, 1)
-    B_p = np.array([1]).reshape(-1, 1)
-    C_p = np.array([1]).reshape(1, -1)
-    D_p = np.array([0]).reshape(1, 1)
     plant_model = StateSpaceModel(A_p, B_p, C_p, D_p)
-
-    A_m = np.array([-2]).reshape(-1, 1)
-    B_m = np.array([2]).reshape(-1, 1)
-    C_m = np.array([1]).reshape(1, -1)
-    D_m = np.array([0]).reshape(1, 1)
     reference_model = StateSpaceModel(A_m, B_m, C_m, D_m)
-
-
-    dt = 0.01
-    t = np.arange(0, 20, dt)
-    
-    # step to mag 1 for 10 seconds, then -1 for 10 seconds
-    r = np.ones(t.shape) * 1.0
-    r[t > 10] = -1.0
-
-
     simulator = MRACSimulator(plant_model, reference_model, gamma=5.0)
-    x_p, x_m, u, theta_hist = simulator.run_simulation(t, r, dt)
-    simulator.plot_results(t, x_p, x_m, u, theta_hist)
 
+    # Plotting setup
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 8))
+    plt.subplots_adjust(bottom=0.25)
+    line_ref, = ax1.plot([], [], label='Reference $x_m$', color='blue')
+    line_plant, = ax1.plot([], [], label='Plant $x_p$', linestyle='--', color='red')
+    ax1.set_xlim(0, 10)
+    ax1.set_ylim(-6, 6)
+    ax1.legend()
+    ax1.set_ylabel('Output')
 
+    line_u, = ax2.plot([], [], label='Control $u$', color='orange')
+    ax2.set_xlim(0, 10)
+    ax2.set_ylim(-10, 10)
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Control Input')
+    ax2.legend()
+
+    line_theta0, = ax3.plot([], [], label=r'$\theta_r$', linestyle='--', color='purple')
+    line_theta1, = ax3.plot([], [], label=r'$\theta_{x_p}$', linestyle=':', color='green')
+    ax3.set_xlim(0, 10)
+    ax3.set_ylim(-20, 20)
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Adaptive Parameters')
+    ax3.legend()
+
+    slider_ax = plt.axes([0.25, 0.05, 0.50, 0.03])
+    slider = Slider(slider_ax, 'Reference Input', -5.0, 5.0, valinit=1.0)
+
+    # Simulation buffers
+    dt = 0.05
+    t_data, x_p_data, x_m_data, u_data, theta_0_data, theta_1_data = [], [], [], [], [], []
+    time_counter = 0
+
+    def update(frame):
+        global time_counter
+        r = slider.val
+        x_p, x_m, u, theta = simulator.step(r, dt)
+        time_counter += dt
+
+        t_data.append(time_counter)
+        x_p_data.append(x_p)
+        x_m_data.append(x_m)
+        u_data.append(u)
+        theta_0_data.append(theta[0])
+        theta_1_data.append(theta[1])
+
+        t_min = max(0, time_counter - 10)
+        ax1.set_xlim(t_min, time_counter)
+        ax2.set_xlim(t_min, time_counter)
+        ax3.set_xlim(t_min, time_counter)
+
+        line_ref.set_data(t_data, x_m_data)
+        line_plant.set_data(t_data, x_p_data)
+        line_u.set_data(t_data, u_data)
+        line_theta0.set_data(t_data, theta_0_data)
+        line_theta1.set_data(t_data, theta_1_data)
+
+        return line_ref, line_plant, line_u, line_theta0, line_theta1
+
+    ani = FuncAnimation(fig, update, interval=dt*1000)
+    plt.show()
